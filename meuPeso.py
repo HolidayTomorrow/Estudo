8a4f2633c6058f0c6e74bb691e2bcdadb60a3f3f
HolidayTomorrow <marcelo.rlopes@hotmail.com>
2024-08-26 14:04:57 -0300

    Alterações para arquivos informando meu peso

---
+++ meuPeso.py
@@ -0,0 +1,13 @@
+nome = 'Marcelo dos Santos Rigobeli Rocha'
+peso = 77.5
+altura = 1.66
+imc = peso / altura ** 2
+
+"f-strings"
+write = f'{nome} tem {altura:.2f} de altura.'
+writePeso = f'pesa {peso} quilos e seu imc é: '
+writeImc = f'{imc:.2f}'
+
+print(write)
+print(writePeso)
+print(writeImc)

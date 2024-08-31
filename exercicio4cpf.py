bc3647b37577eae5314f4476b35c169fce8823be
HolidayTomorrow <marcelo.rlopes@hotmail.com>
2024-08-31 16:05:58 -0300

    Correção para situaçao de dígitos repetidos no exercício.

--- exercicio4cpf.py
+++ exercicio4cpf.py
@@ -25,33 +25,46 @@ condicao = True
 while condicao:
     multiplicadores_primeiro_digito = 10
     multiplicadores_segundo_digito = 11
-    # cpf_digitado = '74682489070' # Dados de entrada como teste para o exercício.
+    # cpf_digitado = '746.824.890-70' # Dados de entrada como teste para o exercício.
+    # cpf_digitado = '111.111.111-11'
     cpf_digitado = input('CPF: ')
     cpf_digitado = re.sub(r'[^0-9]', '', cpf_digitado) # Operação nova até agora. Envolve a biblioteca "re" e opera dentro de uma string com números.
     # O método re.sub "re.sub(r'[^0-9]', '', cpf_digitado)" faz a string ('cpf_digitado') eliminar dados diferentes de números entre 0 e 9, colocando nada ('') em seu lugar.
     cpf_coletado = cpf_digitado[:9]
     resultado = 0
-    for digito_cpf in cpf_coletado:
-        resultado += int(digito_cpf)*multiplicadores_primeiro_digito
-        multiplicadores_primeiro_digito -= 1
-    resultado = ((resultado*10) % 11) if ((resultado*10) % 11) <= 9 else 0 # Operação ternária em Python. Melhor coisa que aprendi até aqui!
-    cpf_coletado += str(resultado)
-    resultado = 0
-    for digito_cpf in cpf_coletado:
-        resultado += int(digito_cpf) * multiplicadores_segundo_digito
-        multiplicadores_segundo_digito -= 1
-    resultado = ((resultado*10) % 11) if ((resultado*10) % 11) <= 9 else 0
-    cpf_coletado += str(resultado)
-    if cpf_coletado == cpf_digitado:
-        os.system('cls')
-        print('CPF Válido!')
-    elif cpf_coletado != cpf_digitado:
+    digito_repetido = True if cpf_coletado == cpf_coletado[0]*len(cpf_coletado) else False # Foi necessário acrescentar verificação de cpf com uma bandeira bool.
+    if digito_repetido == False:
+        for digito_cpf in cpf_coletado:
+            resultado += int(digito_cpf)*multiplicadores_primeiro_digito
+            multiplicadores_primeiro_digito -= 1
+        resultado = ((resultado*10) % 11) if ((resultado*10) % 11) <= 9 else 0 # Operação ternária em Python. Melhor coisa que aprendi até aqui!
+        cpf_coletado += str(resultado)
+        resultado = 0
+        for digito_cpf in cpf_coletado:
+            resultado += int(digito_cpf) * multiplicadores_segundo_digito
+            multiplicadores_segundo_digito -= 1
+        resultado = ((resultado*10) % 11) if ((resultado*10) % 11) <= 9 else 0
+        cpf_coletado += str(resultado)
+        if cpf_coletado == cpf_digitado:
+            os.system('cls')
+            print('CPF Válido!')
+        elif cpf_coletado != cpf_digitado:
+            os.system('cls')
+            print('CPF Inválido!')
+        continuar = input('Deseja digitar mais CPF? (s/n): ')
+        if continuar == 's' or continuar == 'S':
+            continue
+        elif continuar == 'n' or continuar == 'N':
+            condicao = False
+            os.system('cls')
+            print('Fim.')
+    elif digito_repetido == True:
         os.system('cls')
         print('CPF Inválido!')
-    continuar = input('Deseja digitar mais CPF? (s/n): ')
-    if continuar == 's' or continuar == 'S':
-        continue
-    elif continuar == 'n' or continuar == 'N':
-        condicao = False
-        os.system('cls')
-        print('Fim.')
+        continuar = input('Deseja digitar mais CPF? (s/n): ')
+        if continuar == 's' or continuar == 'S':
+            continue
+        elif continuar == 'n' or continuar == 'N':
+            condicao = False
+            os.system('cls')
+            print('Fim.')

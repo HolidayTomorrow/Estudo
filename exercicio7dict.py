8bbb9a92a398d89e58addd3fdf08d6479c9d6953
HolidayTomorrow <marcelo.rlopes@hotmail.com>
2024-09-04 07:59:07 -0300

    Exercício envolvendo dicionários. Primeira vez apredendo sobre dicionários.

--- exercicio7dict.py
+++ exercicio7dict.py
@@ -12,30 +12,64 @@
 # pop - Apaga um item com a chave especificada (del)
 # popitem - Apaga o último item adicionado
 # update - Atualiza um dicionário com outro
-
+import os
 perguntas = [
     {
         'Pergunta': 'Quanto é 2+2?',
         'Opções': ['1','3','4','5'],
+        'Resposta': '4',
     },
     {
         'Pergunta': 'Quanto é 5*5?',
         'Opções': ['25','55','10','51'],
+        'Resposta': '25',
     },
     {
         'Pergunta': 'Quanto é 10/2?',
         'Opções': ['4','5','2','1'],
+        'Resposta': '5',
     },
 ]
 
 condicao = True
 while condicao:
     iniciar = input('Deseja responder as perguntas?(s/n) ')
+    os.system('cls')
     if iniciar == 'n' or iniciar == 'N':
         condicao = False
+        os.system('cls')
+        print('Fim.')
     else:
+        acertos = 0
         for pergunta in perguntas:
-            for valor in pergunta.values():
-                print('Escolha a alternativa correta')
-                
-                print(valor)
+            print('\nEscolha a alternativa correta\n')
+
+            print('Pergunta: ', pergunta['Pergunta'])
+
+            opcoes = pergunta['Opções']
+            for i, opcao in enumerate(opcoes):
+                print(f'{i})', opcao)
+
+            escolha = input('Escoha uma opção: ')
+
+            acertou = False
+            qtd_opcoes = len(opcoes)
+
+            if escolha.isdigit():
+                escolha = int(escolha)
+
+            if escolha is not None:
+                if escolha >= 0 and escolha < qtd_opcoes:
+                    if opcoes[escolha] == pergunta['Resposta']:
+                        acertou = True
+            
+            print()
+            if acertou:
+                acertos += 1
+                print('Acertou 👍')
+            else:
+                print('Errou ❌')
+
+            print()
+        print('Você acertou', acertos)
+        print('de', len(perguntas), 'perguntas.')

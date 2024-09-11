2d1b739b5b103b045ab97810b44036b07a283505
HolidayTomorrow <marcelo.rlopes@hotmail.com>
2024-09-11 07:49:20 -0300

    Alterações com correções nos códigos e arquivos base do visual studio.

--- .ipynb_checkpoints/CalcFeetToMetersTtk-checkpoint.py
+++ .ipynb_checkpoints/CalcFeetToMetersTtk-checkpoint.py
@@ -1,14 +1,12 @@
 from tkinter import *
 from tkinter import ttk
 
-
 def calculate(*args):
     try:
         value = float(feet.get())
-        meters.set(int(0.3048 * value * 10000.0 + 0.5) / 10000.0)
+        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
     except ValueError:
         pass
-
 
 root = Tk()
 root.title("Feet to Meters")
@@ -31,7 +29,7 @@ ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
 ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
 ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
 
-for child in mainframe.winfo_children():
+for child in mainframe.winfo_children(): 
     child.grid_configure(padx=5, pady=5)
 
 feet_entry.focus()

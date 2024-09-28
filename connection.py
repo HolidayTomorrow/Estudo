3c4d8ce1ecb8d45fd2a2fa2b455756a5c51b62b0
HolidayTomorrow <marcelo.rlopes@hotmail.com>
2024-09-28 18:56:09 -0300

    Adição do método create_with_auth.

--- connection.py
+++ connection.py
@@ -21,7 +21,15 @@ class Connection:
     def set_password(self, password):
         self.password = password
         
-        
+    @classmethod
+    def create_with_auth(cls, user, password):
+        connection = cls()
+        connection.user = user
+        connection.password = password
+        return connection
+    
+    
+    
 c1 = Connection()
 c1.set_user('Luiz')
 print(c1.user)

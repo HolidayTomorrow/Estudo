393734507ba540753b55c1713f7e1d1d0dbc7f4e
HolidayTomorrow <marcelo.rlopes@hotmail.com>
2024-09-28 18:49:12 -0300

    Adição do método set_password.

--- connection.py
+++ connection.py
@@ -18,6 +18,9 @@ class Connection:
         #Setter
         self.user = user
         
+    def set_password(self, password):
+        self.password = password
+        
         
 c1 = Connection()
 c1.set_user('Luiz')

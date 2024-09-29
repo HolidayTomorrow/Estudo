dc6cb740f4fd1e9c42ded78c13b3543e8115a076
HolidayTomorrow <marcelo.rlopes@hotmail.com>
2024-09-29 12:43:51 -0300

    Atualização dos métodos decorados com utilização de setter e getter.

--- connection.py
+++ connection.py
@@ -6,20 +6,33 @@
 
 
 
-
-
 class Connection:
-    def __init__(self,host= 'localhost'):
+    def __init__(self, host = 'localhost'):
         self.host = host
-        self.user = None
-        self.password = None
+        self._user = None
+        self._password = None
+    @property
+    def user(self):
+        return self._user
+    @property    
+    def password(self):
+        return self._password
+    
+    @user.setter
+    def setUser(self, user):
+        self._user = user
         
-    def set_user(self, user):
-        #Setter
-        self.user = user
+    @user.getter
+    def getUser(self):
+        return self._user
         
-    def set_password(self, password):
-        self.password = password
+    @password.setter
+    def setPassword(self, password):
+        self._password = password
+    
+    @user.getter
+    def getPassword(self):
+        return self._password
         
     @classmethod
     def create_with_auth(cls, user, password):
@@ -31,5 +44,33 @@ class Connection:
     
     
 c1 = Connection()
-c1.set_user('Luiz')
-print(c1.user)
+c1.setUser = 'marcelo.rlopes'
+c1.setPassword = 'password123'
+print(c1.getUser)
+print(c1.getPassword)
+
+# class Connection:
+#     def __init__(self,host= 'localhost'):
+#         self.host = host
+#         self.user = None
+#         self.password = None
+        
+#     def set_user(self, user):
+#         #Setter
+#         self.user = user
+        
+#     def set_password(self, password):
+#         self.password = password
+        
+#     @classmethod
+#     def create_with_auth(cls, user, password):
+#         connection = cls()
+#         connection.user = user
+#         connection.password = password
+#         return connection
+    
+    
+    
+# c1 = Connection()
+# c1.set_user('Luiz')
+# print(c1.user)

96d217e6d0aa377e57f258db744bc5b25b0debb1
HolidayTomorrow <marcelo.rlopes@hotmail.com>
2024-10-02 14:56:47 -0300

    Alteração na linha 6, acrescentando e executando o Path da pathlib.

--- Web_Scraping/charUniCode.py
+++ Web_Scraping/charUniCode.py
@@ -1,9 +1,11 @@
 import chardet
+from pathlib import Path
 import os
 
-local = os.path.dirname(__file__)
+# local = os.path.dirname(__file__)
+local = Path(__file__).parent
 # local = "c:\\Users\\marcelo.rlopes\\Documents\\Estudos\\Estudo\\Python\\Web_Scraping\\"
-
+# print(local)
 
 with open(local+ '\\text.txt', 'rb') as file:
     raw_content = file.read()
@@ -16,7 +18,7 @@ with open(local+ '\\text.txt', 'rb') as file:
     raw_content = file.read()
     content_string = raw_content.decode(encoding["encoding"])
     print('\n',content_string)
-    
-    
+
+
 with open(local+ '\\text2.txt', 'wb') as file:
     file.write(content_string.encode('utf8'))

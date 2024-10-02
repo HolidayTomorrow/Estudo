e7bad4a60088ff36ac0166b4b5161c26fc39b434
HolidayTomorrow <marcelo.rlopes@hotmail.com>
2024-10-02 12:53:46 -0300

    Primeiro programa envolvendo UniCode com a biblioteca chardet.

---
+++ Web_Scraping/charUniCode.py
@@ -0,0 +1,22 @@
+import chardet
+import os
+
+local = os.path.dirname(__file__)
+# local = "c:\\Users\\marcelo.rlopes\\Documents\\Estudos\\Estudo\\Python\\Web_Scraping\\"
+
+
+with open(local+ '\\text.txt', 'rb') as file:
+    raw_content = file.read()
+    encoding = chardet.detect(raw_content)
+    encoding["encoding"]
+    print(encoding)
+
+
+with open(local+ '\\text.txt', 'rb') as file:
+    raw_content = file.read()
+    content_string = raw_content.decode(encoding["encoding"])
+    print('\n',content_string)
+    
+    
+with open(local+ '\\text2.txt', 'wb') as file:
+    file.write(content_string.encode('utf8'))
